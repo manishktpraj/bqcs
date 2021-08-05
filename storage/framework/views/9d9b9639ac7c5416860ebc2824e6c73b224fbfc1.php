82618cb62131fb673f8526fd1040e16d3332aae5
Manish Garg <manishktpraj@gmail.com>
2021-08-05 19:14:54 +0530

    mgf

--- storage/framework/views/9d9b9639ac7c5416860ebc2824e6c73b224fbfc1.php
+++ storage/framework/views/9d9b9639ac7c5416860ebc2824e6c73b224fbfc1.php
@@ -1,13 +1,29 @@
-<link rel="stylesheet" href="<?php echo SITE_ASSETS_URL;?>css/style.css">
+<!doctype html>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport"
+content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover" />
+<meta name="apple-mobile-web-app-capable" content="yes" />
+<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+<meta name="theme-color" content="#000000">
+<title>Building & Pest Inspection</title>
+<meta name="description" content="Mobilekit HTML Mobile UI Kit">
+<meta name="keywords" content="bootstrap 5, mobile template, cordova, phonegap, mobile, html" />
+<link rel="icon" type="image/png" href="<?php echo SITE_ASSETS_URL?>img/favicon.png" sizes="32x32">
+<link rel="apple-touch-icon" sizes="180x180" href="assets/img/icon/192x192.png">
+<link rel="stylesheet" href="<?php echo SITE_ASSETS_URL?>css/style.css">
 <link rel="manifest" href="__manifest.json">
+<link href="<?php echo SITE_ASSETS_URL?>js/lib/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet">
+<link href="<?php echo SITE_ASSETS_URL?>js/lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+<link href="<?php echo SITE_ASSETS_URL?>js/lib/fullcalendar/fullcalendar.min.css" rel="stylesheet">
+<link rel="stylesheet" href="<?php echo SITE_ASSETS_URL?>css/dashforge.calendar.css">
 </head>
 <body>
 <!-- loader -->
 <div id="loader">
 <div class="spinner-border text-primary" role="status"></div>
 </div>
-<!-- * loader -->
-<!-- App Header -->
 <div class="appHeader bg-primary text-light">
 <div class="left">
 <a href="#" class="headerButton" data-bs-toggle="modal" data-bs-target="#sidebarPanel">
